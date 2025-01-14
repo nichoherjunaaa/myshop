04b36786b480c15fcf1d60851e1a28dba736f802
nichoherjunaa <nichoherjunaa@gmail.com>
2025-01-14 21:53:51 +0700

    create navbar

--- client/src/layouts/PublicLayout.jsx
+++ client/src/layouts/PublicLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
 import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 const PublicLayout = () => {
     return (
         <>
             <Header />
+            <Navbar />
             <main className="mx-auto max-w-6xl px-8 py-20 min-h-80">
                 <Outlet />
             </main>
