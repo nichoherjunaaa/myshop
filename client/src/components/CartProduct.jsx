c4fdbec0022f15c498de3a49be2e68ec998023a6
nichoherjunaa <nichoherjunaa@gmail.com>
2025-01-20 21:52:35 +0700

    action to delete product

--- client/src/components/CartProduct.jsx
+++ client/src/components/CartProduct.jsx
@@ -1,23 +1,45 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useRevalidator } from 'react-router-dom'
 import { formatHarga } from '../utils'
 import { FaTrash, FaPencilAlt } from 'react-icons/fa'
+import API from '../api'
+import { toast } from 'react-toastify'
 
 const CartProduct = ({ item, user }) => {
-    // console.log(user);
-    
+    const { revalidate } = useRevalidator()
+    const handleDelete = async () => {
+        const confirm = window.confirm('Are you sure to delete this product?')
+        if (confirm) {
+            try {
+                await API.delete(`/product/delete/${item._id}`)
+                toast.success('Product Deleted')
+                revalidate()
+            } catch (error) {
+                toast.error('Product gagal terhapus')
+            }
+        }
+    }
     return (
         <>
             <div className="card bg-base-300 shadow-xl" key={item._id}>
                 <figure>
-                    <img
-                        src={item.image}
-                        alt="Shoes" />
+                    <div className="relative">
+
+
+                        <img
+                            src={item.image}
+                            alt="Shoes" />
+                        {
+                            item.stock < 1 && (
+                                <span className="absolute top-0 right-0 bg-error text-xl rounded-md font-bold p-2">Sold Out</span>
+                            )
+                        }
+                    </div>
                 </figure>
                 <div className="card-body">
                     {user && user.role === "owner" && (
                         <div className="flex justify-end gap-x-3">
-                            <FaTrash className="text-red-500 cursor-pointer" />
+                            <FaTrash onClick={handleDelete} className="text-red-500 cursor-pointer" />
                             <Link to={`/product/${item._id}/edit`}><FaPencilAlt className="text-info cursor-pointer" /></Link>
                         </div>
                     )}
